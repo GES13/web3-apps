47f68e781d2c7a71da7c647d1e35d0f0af0965f2
Erix <guntur.erix@gmail.com>
2022-02-13 00:06:26 +0700

    push purchase.ride

--- src/multi-sig.js
+++ src/multi-sig.js
@@ -1,15 +1,15 @@
 const wvs = 10 ** 8;
 const accountBobAddress = "3MvFKVUHr56HAeF755rAT6bUo7HvG9mov8w";
-const accountCooperAddress = "3MrJfsb1qUt9ve1Uqw1UYxU42PRKpk6fPaF";
+const accountStarkAddress = "3MrJfsb1qUt9ve1Uqw1UYxU42PRKpk6fPaF";
 const accountBobPK = "4Ga2KQmJakg2D7bY1LrHxxuEynx12QEVJiMU59pZ5xWS";
-const accountCooperPK = "28HgXAYUDpWqUKGh6afRJk4Q5YCsh53Dgu6JenqVtFJ7";
+const accountSomebodykPK = "7X1UipBUQ7x7HwsHDzFU3SFD39X9f7Euqk68tc4DY9M9";
 
 
 
 describe('some suite', () => {
 
     it('multisig transfer success', async function(){
-        let txObjectSignedAlice = transfer({amount: 100000, recipient: accountBobAddress, fee: 500000 })
+        let txObjectSignedAlice = transfer({amount: 1 * wvs, recipient: accountBobAddress, fee: 500000 })
         let txObjectSignedAliceBob = transfer(txObjectSignedAlice, {privateKey: accountBobPK})
 
         let tx = await broadcast(txObjectSignedAliceBob)
@@ -19,8 +19,8 @@ describe('some suite', () => {
     })
 
     it('multisig transfer failed', async function(){
-        let txObjectSignedAlice = transfer({amount: 100000, recipient: accountCooperAddress, fee: 500000 })
-        let txObjectSignedAliceCooper = transfer(txObjectSignedAlice, {privateKey: accountCooperPK})
+        let txObjectSignedAlice = transfer({amount: 1 * wvs , recipient: accountStarkAddress, fee: 500000 })
+        let txObjectSignedAliceCooper = transfer(txObjectSignedAlice, {privateKey: accountSomebodykPK})
 
         let tx = await broadcast(txObjectSignedAliceCooper)
         await waitForTx(tx.id)
